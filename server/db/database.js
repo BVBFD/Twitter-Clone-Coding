f7c639e1243e17a93e9a85add22bb9b490a89f74
BVBFD <lsevina126@gmail.com>
2022-02-14 02:27:36 +0900

    Code Mongoose server

--- server/db/database.js
+++ server/db/database.js
@@ -1,17 +1,26 @@
-import MongoDb from "mongodb";
+import Mongoose from "mongoose";
 import { config } from "../config.js";
 
-let db;
 export async function connectDB() {
-  return MongoDb.MongoClient.connect(config.db.host) //
-    .then((client) => {
-      db = client.db();
-    });
+  return Mongoose.connect(config.db.host);
+  // { useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useFindAndModify: false, }
+  // mongoose 6.0 이하는 위 옵션 추가 했음.
 }
 
-export function getUsers() {
-  return db.collection("users");
+export function useVirtualId(schema) {
+  schema.virtual("id").get(function () {
+    return this._id.toString();
+  });
+  schema.set("toJSON", { virtuals: true });
+  schema.set("toObject", { virtuals: true });
 }
+
+let db;
+// export function getUsers() {
+//   return db.collection("users");
+// }
 
 export function getTweets() {
   return db.collection("tweets");
